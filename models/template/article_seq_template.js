2510bfd63af8531b6f5f010349e27f272ed9cfc2
Lei Zhu <thunderzhulei@gmail.com>
2012-02-21 17:53:35 +0800

    add *.js, node_modules to gitignore

--- models/template/article_seq_template.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  module.exports = function(seq, DataTypes) {
-    return seq.define('ARTICLE', {
-      title: DataTypes.STRING,
-      body: DataTypes.TEXT
-    });
-  };
-}).call(this);
--- models/template/comment_seq_template.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  module.exports = function(seq, DataTypes) {
-    return seq.define('COMMENT', {
-      body: DataTypes.STRING
-    });
-  };
-}).call(this);
